2c247f356480c6e406aeed30a83077173122df73
apim <a.pimonov@g.nsu.ru>
2023-12-16 23:15:40 +0700

    detector: +green, +intersection (with turns)

--- detect_signs/detect_signs/detect.py
+++ detect_signs/detect_signs/detect.py
@@ -8,7 +8,7 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 
-class SnapSubscriber(Node):
+class Detector(Node):
 	def __init__(self, cwd):
 		super().__init__('snap_subscriber')
 		self.subscription = self.create_subscription(
@@ -18,49 +18,100 @@ class SnapSubscriber(Node):
 			10)
 		self.subscription
 		
+		# Связка OpenCV с ROS'овскими сообщениями
 		self.br = CvBridge()
 		
+		# Флаг, нашли ли то, что искали
+		self.is_found = False
+		# Режимы работы детектора
 		self.modes = {
-			0: '/templates/intersection.png',
-			1: '/templates/turn_left.png'
+			0: 'looking for some green lantern',
+			1: '/templates/intersection.png',
+			2: '/templates/turn_left.png',
+			3: '/templates/turn_right.png'
+			
 		}
+		# Текущий режим работы детектора
+		# (сначала фиксируем зеленый свет)
 		self.curr_mode = 0
+		
+		# Работа с директориями
 		self.cwd = cwd
 		self.images_path = self.cwd + '/src/porosyata/detect_signs/images'
 		self.curr_frame_path = self.images_path + '/curr_frame.png'
-		self.intersection_path = self.images_path + '/templates/turn_left.png'
+		self.curr_template = self.images_path + self.modes[self.curr_mode]
 	
 	def snap_callback(self, msg):
-		is_found = False
-		
+		# Фиксируем текущий кадр
 		img = self.br.imgmsg_to_cv2(msg)
 		img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+		# Записываем его (потом уберу запись, скорее всего)
 		cv2.imwrite(self.curr_frame_path, img)
 		
+		is_found = False
+		
+		# Зеленый свет
+		if self.curr_mode == 0:
+			self.detect_green(img)
+		# Выбор направления поворота
+		# (поиск сразу двух знаков)
+		elif self.curr_mode == 2:
+			self.choose_direction(img)
+		# Просто поиск знаков
+		else:
+			self.detect_sign(img, self.curr_mode)
+		
+		# Если нашли, что искали, переключаем режим
+		if self.is_found:
+			self.curr_mode += 1
+			self.curr_template = self.images_path + self.modes[self.curr_mode]
+			self.is_found = False
+		
+		cv2.imshow('camera', img)
+		cv2.waitKey(1)
+	
+	def detect_green(self, img):
+		lower = np.array([0, 40, 0], dtype="uint8")
+		upper = np.array([10, 255, 10], dtype="uint8")
+		green_mask = cv2.inRange(img, lower, upper)
+		img_masked = cv2.bitwise_and(img, img, mask=green_mask)
+		
+		if np.any(img_masked[279, 605] != 0):
+			self.is_found = True
+	
+	def detect_sign(self, img, mode):
 		img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-		template = cv2.imread(self.images_path + self.modes[self.curr_mode], 0)
+		template = cv2.imread(self.images_path + self.modes[mode], 0)
 		
 		w, h = template.shape
 		res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
 		threshold = 0.8
 		loc = np.where(res >= threshold)
 		for pt in zip(*loc[::-1]):
-			if not is_found:
-				is_found = True
-				self.curr_mode += 1
+			if not self.is_found:
+				self.is_found = True
 			cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), (255, 0, 150), 2)
+	
+	def choose_direction(self, img):
+		is_left = False
+		is_right = False
 		
-		cv2.imshow('camera', img)
-		cv2.waitKey(1)
+		is_left = self.detect_sign(img, self.curr_mode)
+		is_right = self.detect_sign(img, self.curr_mode + 1)
+		
+		if is_left or is_right:
+			self.is_found = True
+			self.curr_mode += 1
+		
 
 def main(args=None):
 	cwd = os.getcwd()
 	rclpy.init(args=args)
 	
-	snap_subscriber = SnapSubscriber(cwd)
-	rclpy.spin(snap_subscriber)
+	detector = Detector(cwd)
+	rclpy.spin(detector)
 	
-	snap_subscriber.destroy_node()
+	detector.destroy_node()
 	rclpy.shutdown()
 
 if __name__ == '__main__':
