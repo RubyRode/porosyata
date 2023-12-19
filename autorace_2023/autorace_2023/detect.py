adb90ea97161db3a069b04f95850b39930a21fb3
RubyRode <dimaskrut71@gmail.com>
2023-12-19 19:43:04 +0700

    detect: +fix of state switch

--- autorace_2023/autorace_2023/detect.py
+++ autorace_2023/autorace_2023/detect.py
@@ -30,21 +30,21 @@ class Detector(Node):
 		self.is_found = False
 		# Режимы работы детектора
 		self.modes = {
-			0: ('looking for some green lantern', 0),
-			1: ('/templates/intersection.png', 1),
-			2: ('/templates/turn_left.png', 2),
-			3: ('/templates/turn_right.png', 3),
-			4: ('/templates/blocks.png', 4),
-			5: ('going through some blocks', 5),
-			6: ('/templates/parking.png', 6),
-			7: ('/templates/car_left.png', 7),
-			8: ('/templates/car_right.png', 8),
- 			9: ('ryan gosling parking', 9),
-			10: ('/templates/zebra.png', 10),
-			11: ('looking for some knight', 11),
-			12: ('/templates/tunnel.png', 12),
-			13: ('looking for some slam', 13),
-			14: ('finish', 14),
+			0: 'looking for some green lantern',
+			1: '/templates/intersection.png',
+			2: '/templates/turn_left.png',
+			3: '/templates/turn_right.png',
+			4: '/templates/blocks.png',
+			5: 'going through some blocks',
+			6: '/templates/parking.png',
+			7: '/templates/car_left.png',
+			8: '/templates/car_right.png',
+ 			9: 'ryan gosling parking',
+			10: '/templates/zebra.png',
+			11: 'looking for some knight',
+			12: '/templates/tunnel.png',
+			13: 'looking for some slam',
+			14: 'finish',
 			
 		}
 		# Текущий режим работы детектора
@@ -52,7 +52,7 @@ class Detector(Node):
 		self.curr_mode = 0
 		# Сообщение, которое будем публиковать при нахождении того,
 		# что искали
-		self.mode_msg = String()
+		self.mode_msg = Int8()
 		# Сообщение, которое будем получать с камеры глубины
 		
 		# Работа с директориями
@@ -72,50 +72,54 @@ class Detector(Node):
 		# cv2.imwrite(self.curr_frame_path, img)
 		
 		# Зеленый свет
-		mode_msg = Int8()
 		match self.curr_mode:
 			case 0: 
 				if self.detect_green(img):
-					self.curr_mode = 1
+					self.is_found = True
 			case 1:
-				if self.detect_sign(img):
-					self.curr_mode = 2
+				self.detect_sign(img, self.curr_mode)
 			case 2:
 				is_found, is_left = self.choose_from_two(img)
-				if is_found
-					self.curr_mode = 4
+				if is_found:
+					self.is_found = True
+					if not is_left:
+						self.curr_mode += 1
 			case 4:
-				if self.detect_sign(img):
-					self.curr_mode = 5
+				self.detect_sign(img, self.curr_mode)
 			case 5:
 				# hardcode blocks
-				self.curr_mode = 6
+				self.is_found = True
 			case 6:
-				if self.detect_sign(img):
-					self.curr_mode = 7
+				self.detect_sign(img, self.curr_mode)
 			case 7:
-				if self.choose_from_two(img):
-					self.curr_mode = 9
+				is_found, is_left = self.choose_from_two(img)
+				if is_found:
+					self.is_found = True
+					if not is_left:
+						self.curr_mode += 1
 			case 9:
 				# hardcode parking
-				self.curr_mode = 10
+				self.is_found = True
 			case 10:
-				if self.detect_sign(img):
-					self.curr_mode = 11
+				self.detect_sign(img, self.curr_mode)
 			case 11:
 				if self.detect_pedestrian(img):
-					self.curr_mode = 12
+					self.is_found = True
 			case 12:
-				if self.detect_sign(img):
-					self.curr_mode = 13
+				self.detect_sign(img, self.curr_mode)
 			case 13:
 				# hardcode slam
-				self.curr_mode = 14
+				self.is_found = True
 			case 14:
-				self.curr_mode = self.curr_mode
-		mode_msg.data = self.curr_mode
-		self.mode_publisher.publish(mode_msg)
-		# self.get_logger().info(f"/detect/signs/mode: {mode_msg.data}, {self.curr_mode}")
+				self.is_found = True
+
+		if self.is_found:
+			self.curr_mode += 1
+			self.mode_msg.data = self.curr_mode
+			self.is_found = False
+
+		self.mode_publisher.publish(self.mode_msg)
+		#self.get_logger().info(f"/detect/signs/mode: {self.mode_msg.data}, {self.curr_mode}")
 
 
 
@@ -168,14 +172,11 @@ class Detector(Node):
 		res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
 		threshold = 0.8
 		loc = np.where(res >= threshold)
-		if len(loc) > 0:
-			is_found = True
-			mode_msg.data = str(self.curr_mode)
-		else:
-			is_found = False
 		for pt in zip(*loc[::-1]):
+			if not self.is_found:
+				self.is_found = True
 			cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), (255, 0, 150), 2)
-		return is_found
+		return self.is_found
 	
 	def choose_from_two(self, img):
 		is_left = False
@@ -186,9 +187,9 @@ class Detector(Node):
 		
 		if is_left or is_right:
 			if is_left:
-				self.mode_msg.data = str(self.curr_mode)
+				self.mode_msg.data = self.curr_mode
 			else:
-				self.mode_msg.data = str(self.curr_mode + 1)
+				self.mode_msg.data = self.curr_mode + 1
 			return True, is_left
 
 		return False, False
@@ -199,7 +200,7 @@ class Detector(Node):
 		right = 520
 		
 		if np.all(img[row, 330:520] == img[row, 330]):
-			self.mode_msg.data = str(self.curr_mode)
+			self.mode_msg.data = self.curr_mode
 			return True
 
 		return False
