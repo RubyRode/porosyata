7e21300f6e5227ab3838f27e309fbc56701420e8
RubyRode <dimaskrut71@gmail.com>
2023-12-21 14:08:36 +0700

    now images are also dragged to /ws/install

--- autorace_2023/setup.py
+++ autorace_2023/setup.py
@@ -12,6 +12,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'images', 'templates'), glob(os.path.join('images', 'templates', '*.png'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
